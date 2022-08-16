648464db42e27f0bccba80aefbfb4fc26f87ce7c
tencentcloudapi <tencentcloudapi@tencent.com>
2022-08-16 13:01:37 +0800

    release v3.0.514

--- tencentcloud/common/http/request.go
+++ tencentcloud/common/http/request.go
@@ -254,7 +254,7 @@ func CompleteCommonParams(request Request, region string) {
 	params["Action"] = request.GetAction()
 	params["Timestamp"] = strconv.FormatInt(time.Now().Unix(), 10)
 	params["Nonce"] = strconv.Itoa(rand.Int())
-	params["RequestClient"] = "SDK_GO_3.0.513"
+	params["RequestClient"] = "SDK_GO_3.0.514"
 }
 
 func ConstructParams(req Request) (err error) {
