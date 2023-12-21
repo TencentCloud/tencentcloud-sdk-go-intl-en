19f977a72db4d255785d7d49e04e1b2ce2844b0c
tencentcloudapi <tencentcloudapi@tencent.com>
2023-12-22 05:11:31 +0800

    release v3.0.862

--- tencentcloud/faceid/v20180301/models.go
+++ tencentcloud/faceid/v20180301/models.go
@@ -233,36 +233,40 @@ func (r *ApplySdkVerificationTokenResponse) FromJsonString(s string) error {
 
 // Predefined struct for user
 type ApplyWebVerificationBizTokenIntlRequestParams struct {
-	// The Base64-encoded string (max 8 MB in size) of the photo to be compared.
-	CompareImageBase64 *string `json:"CompareImageBase64,omitnil" name:"CompareImageBase64"`
-
 	// The web callback URL to redirect to after the verification is completed, including the protocol, hostname, and path. 
-	// Example: https://www.tencentcloud.com/products/faceid.
 	// After the verification process is completed, the BizToken of this process will be spliced to the callback URL in the format of https://www.tencentcloud.com/products/faceid?token={BizToken} before redirect.
+	// Example: https://www.tencentcloud.com/products/faceid.
 	RedirectURL *string `json:"RedirectURL,omitnil" name:"RedirectURL"`
+
+	// The Base64-encoded string (max 8 MB in size) of the photo to be compared.The Data URI scheme header needs to be removed from the encoded string
+	// Example: xhBQAAACBjSFJNAAB6****AAAASUVORK5CYII=
+	CompareImageBase64 *string `json:"CompareImageBase64,omitnil" name:"CompareImageBase64"`
 
 	// The passthrough parameter of the business, max 1,000 characters, which will be returned in GetWebVerificationResultIntl.
 	Extra *string `json:"Extra,omitnil" name:"Extra"`
 
 	// The parameter control the page configuration.
+	// Example: {"AutoSkip": true,"CheckMode": 1,"IdCardType": "HKIDCard"}
 	Config *WebVerificationConfigIntl `json:"Config,omitnil" name:"Config"`
 }
 
 type ApplyWebVerificationBizTokenIntlRequest struct {
 	*tchttp.BaseRequest
 	
-	// The Base64-encoded string (max 8 MB in size) of the photo to be compared.
-	CompareImageBase64 *string `json:"CompareImageBase64,omitnil" name:"CompareImageBase64"`
-
 	// The web callback URL to redirect to after the verification is completed, including the protocol, hostname, and path. 
-	// Example: https://www.tencentcloud.com/products/faceid.
 	// After the verification process is completed, the BizToken of this process will be spliced to the callback URL in the format of https://www.tencentcloud.com/products/faceid?token={BizToken} before redirect.
+	// Example: https://www.tencentcloud.com/products/faceid.
 	RedirectURL *string `json:"RedirectURL,omitnil" name:"RedirectURL"`
+
+	// The Base64-encoded string (max 8 MB in size) of the photo to be compared.The Data URI scheme header needs to be removed from the encoded string
+	// Example: xhBQAAACBjSFJNAAB6****AAAASUVORK5CYII=
+	CompareImageBase64 *string `json:"CompareImageBase64,omitnil" name:"CompareImageBase64"`
 
 	// The passthrough parameter of the business, max 1,000 characters, which will be returned in GetWebVerificationResultIntl.
 	Extra *string `json:"Extra,omitnil" name:"Extra"`
 
 	// The parameter control the page configuration.
+	// Example: {"AutoSkip": true,"CheckMode": 1,"IdCardType": "HKIDCard"}
 	Config *WebVerificationConfigIntl `json:"Config,omitnil" name:"Config"`
 }
 
@@ -278,8 +282,8 @@ func (r *ApplyWebVerificationBizTokenIntlRequest) FromJsonString(s string) error
 	if err := json.Unmarshal([]byte(s), &f); err != nil {
 		return err
 	}
-	delete(f, "CompareImageBase64")
 	delete(f, "RedirectURL")
+	delete(f, "CompareImageBase64")
 	delete(f, "Extra")
 	delete(f, "Config")
 	if len(f) > 0 {
@@ -291,14 +295,17 @@ func (r *ApplyWebVerificationBizTokenIntlRequest) FromJsonString(s string) error
 // Predefined struct for user
 type ApplyWebVerificationBizTokenIntlResponseParams struct {
 	// The token identifying this web-based verification process, valid for 7,200s after issuance. It is required for getting the result after the verification process is completed.
+	// Example: https://intl.faceid.qq.com/reflect/?token=81EEF678-28EE-4759-A82E-6CBBBE6BC442
 	//
 	// Deprecated: VerificationUrl is deprecated.
 	VerificationUrl *string `json:"VerificationUrl,omitnil" name:"VerificationUrl"`
 
 	// The token for the web-based verification, which is generated using the ApplyWebVerificationBizTokenIntl API.
+	// Example: 81EEF678-28EE-4759-A82E-6CBBBE6BC442
 	BizToken *string `json:"BizToken,omitnil" name:"BizToken"`
 
 	// The verification URL to be opened with a browser to start the verification process.
+	// Example: https://intl.faceid.qq.com/reflect/?token=81EEF678-28EE-4759-A82E-6CBBBE6BC442
 	VerificationURL *string `json:"VerificationURL,omitnil" name:"VerificationURL"`
 
 	// The unique request ID, which is returned for each request. RequestId is required for locating a problem.
